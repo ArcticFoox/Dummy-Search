18aa347e93e06919303e903da05ea3ab0ffbdbac
신종신 <jongsin16@naver.com>
2024-08-28 21:58:11 +0900

    first commit

---
+++ src/main/java/org/example/dummysearch/DummySearchApplication.java
@@ -0,0 +1,13 @@
+package org.example.dummysearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DummySearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DummySearchApplication.class, args);
+    }
+
+}
